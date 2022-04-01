b3f1385b65cc92ed342cdd289f44e84f07a7fbc5
egidiodecaro <egidiodecaro@gmail.com>
2022-04-01 12:10:17 +0200

    add line for develop branch

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Master");
+		System.out.println("Now we're on branch develop");
+
 	}
 
 }
